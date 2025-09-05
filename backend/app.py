4492bd4ca3b788ce84dbe61cacac8d970b208425
Ivan Shanygin <shanygin101@yandex.ru>
2025-09-05 23:26:21 +0300

    Fix problems, improve calculate_handler

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,39 @@
-from flask import Flask, jsonify, request
-from calculate import calculate
+from flask import Flask, request, jsonify
+from typing import Any
 
 app = Flask(__name__)
 
-@app.route('/v1/calculate', methods=['POST'])
+bad_expression_msg = "Bad expression"
+json_error_msg = "Request must be JSON"
 
-def calculate():
+
+def calculate(expr: str) -> tuple[bool, Any]:
+    try:
+        answer = eval(expr)
+        result = True
+        return result, answer
+    except BaseException as e:
+        print(f"eval error: {e}")
+        return False, None
+
+
+@app.route("/v1/calculate", methods=["POST"])
+def calculate_handler():
     if not request.is_json:
-        return jsonify({"error": "Request must be JSON"}), 400
+        return jsonify({"error": json_error_msg}), 400
 
-    expr = request.get_json().get('expression')
+    expr = request.get_json().get("expression")
     result, answer = calculate(expr)
     if result:
-        return jsonify({"result": str(answer)})
-    
-    return None, 400
+        return jsonify({"result": str(answer)}), 200
+    else:
+        return jsonify({"error": bad_expression_msg}), 400
 
 
-@app.route("/history")
-def get_history():
+@app.route("/v1/history", methods=["GET"])
+def history_handler():
     pass
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- backend/calculate.py
+++
@@ -1,9 +0,0 @@
-def calculate(expr: str) -> tuple:
-    try:
-        ans = eval(expr)
-        result = True
-    except BaseException as e:
-        ans = 0 
-        result = False
-        print(e)
-    return (result, ans)
